c77decad3a2ba287b5d7f67faeb78a3ce1c67fc6
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-08 22:56:12 +0900

    overlayメニューが表示されるようにする

--- js/main.js
+++ js/main.js
@@ -5,11 +5,12 @@
     const overlay = document.querySelector('.overlay');
 
     open.addEventListener('click', () => {
+        overlay.classList.add('show');
+        open.classList.add('hide');
 
-        
 
 
-    })
+    });
 
 
 }
